a823154beb3e187a535a214fd873193fd69f77cd
Saklain Mostak <saklainmostak926@gmail.com>
2025-02-08 20:08:59 +0600

    js1 done

--- Milestone_4 Hello Javascript/Module18 Fundamentals of conditionals/practice task/p.js
+++ Milestone_4 Hello Javascript/Module18 Fundamentals of conditionals/practice task/p.js
@@ -7,7 +7,7 @@
 //     console.log('Coke 30tk')
 // }
 
-// p2
+// // p2
 
 // let weight = 60;
 // let height = 1.9;
@@ -28,25 +28,85 @@
 // }
 
 
-// p3
+// // p3
 
-let number = 22;
-
-
-if(number >= 90 && number <=100) {
-    console.log("A")
-}
-else if(number >= 80 && number <=89) {
-    console.log("B")
-}
-else if(number >= 70 && number <=79) {
-    console.log("C")
-}
-else if(number >= 60 && number <=69) {
-    console.log("D")
-}
-else{
-    console.log("F")
-}
+// let number = 66;
 
 
+// if(number >= 90 && number <=100) {
+//     console.log("A")
+// }
+// else if(number >= 80 && number <=89) {
+//     console.log("B")
+// }
+// else if(number >= 70 && number <=79) {
+//     console.log("C")
+// }
+// else if(number >= 60 && number <=69) {
+//     console.log("D")
+// }
+// else{
+//     console.log("F")
+// }
+
+// // p4
+// var myMarks = 87;
+// var friendsMark = 33;
+// if(myMarks > 80){
+//     if(friendsMark > 80){
+//         console.log("Go for lunch")
+//     }
+//     else if (friendsMark <80 && friendsMark > 60){
+//         console.log("good luck next time")
+//     }
+//     else if (friendsMark <60 && friendsMark > 40){
+//         console.log("keeping my friend's message unseen")
+//     }
+//     else{
+//         console.log("blocking my friend")
+//     }
+// }
+// else{
+//     console.log("go to home and sleep and act sad")
+// }
+
+
+// // p5
+// var num1 = 66;
+// var num2 = 55;
+// // var result
+// if(num1>num2){
+//     console.log("result: ",result = num1*2)
+// }
+// else{
+//     console.log("result: ",result = num1 + num2)
+// }
+
+// // p5-ternary
+
+// var num1 = 66;
+// var num2 = 55;
+// var result = num1 > num2 ? num1*2 : num1+num2;
+// console.log(result)
+
+
+
+// // p6
+
+// var fare = 800;
+// var age = 71;
+
+// if(age < 10){
+//     console.log("Children so fare is not applicable")
+// }
+// else if(age >= 10 && age <= 28) {
+//     discount = fare * (50/100);
+//     console.log("Student so Fare: ",discount)
+// }
+// else if(age >= 60) {
+//     discount = fare * (85/100);
+//     console.log("Senior Citizen so Fare: ",discount)
+// }
+// else{
+//     console.log("Regular ticket fare 800 tk")
+// }
